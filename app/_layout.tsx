229434390457c640fd154673b0f800e8e8bf72d1
Waliullah <waliullah.godev@gmail.com>
2026-08-04 12:01:36 +0600

    feat(layout): add screenOptions to Stack components for consistent header visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ if (!publishableKey) {
 export default function RootLayout() {
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack />
+      <Stack screenOptions={{headerShown: false}} />
     </ClerkProvider>
   )
 }
---
+++ components/SafeAreaView.tsx
@@ -0,0 +1,7 @@
+import { styled } from 'nativewind'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+
+
+export default styled(SafeAreaView)
