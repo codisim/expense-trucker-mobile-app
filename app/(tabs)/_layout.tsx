954576dd121bdc37e061fb9e05b8fccab3bce29c
Waliullah <waliullah.godev@gmail.com>
2026-08-06 20:09:53 +0600

    feat(tabs): add additional tab triggers for Stats, Add, Expense, and Profile

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,9 +15,26 @@ export default function TabLayout() {
   }
 
   return <NativeTabs>
+
     <NativeTabs.Trigger>
       <Label>Stats</Label>
       <Icon sf={{ default: "chart.bar", selected: "chart.bar.fill" }} />
     </NativeTabs.Trigger>
+
+    <NativeTabs.Trigger>
+      <Label>Add</Label>
+      <Icon sf={{ default: "plus.circle", selected: "plus.circle.fill" }} />
+    </NativeTabs.Trigger>
+
+    <NativeTabs.Trigger>
+      <Label>Expense</Label>
+      <Icon sf={{ default: "creditcard", selected: "creditcard.fill" }} />
+    </NativeTabs.Trigger>
+
+    <NativeTabs.Trigger>
+      <Label>Profile</Label>
+      <Icon sf={{ default: "person", selected: "person.fill" }} />
+    </NativeTabs.Trigger>
+
   </NativeTabs>
 }
