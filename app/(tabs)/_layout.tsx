8568ceb56dc547e582a47729aa42e8665c2c892f
Waliullah <waliullah.godev@gmail.com>
2026-08-06 19:40:34 +0600

    feat(tabs): add TabLayout and HomeScreen components for tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,15 @@
 import { useAuth } from '@clerk/expo'
 import { Redirect, Stack } from 'expo-router'
 
-export default function AuthRoutesLayout() {
+export default function TabLayout() {
   const { isSignedIn, isLoaded } = useAuth()
 
   if (!isLoaded) {
     return null
   }
 
-  if (isSignedIn) {
-    return <Redirect href={"/"} />
+  if (!isSignedIn) {
+    return <Redirect href={"/(auth)/sign-in"} />
   }
 
   return <Stack screenOptions={{headerShown: false}} />
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+
+
+import SafeAreaView from '@/components/SafeAreaView'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView>
+
+
+            <View>
+                <Text >Home Screen</Text>
+            </View>
+
+
+        </SafeAreaView>
+    )
+}
