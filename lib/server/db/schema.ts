d68d4dbe7e0e4270188f058859ca45700d47a189
Waliullah <waliullah.godev@gmail.com>
2026-08-07 01:39:24 +0600

    schema

--- lib/server/db/schema.ts
+++ lib/server/db/schema.ts
@@ -1,7 +1,11 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { bigint, pgTable, text } from "drizzle-orm/pg-core";
 
 
 export const expenseTransaction = pgTable("expense_transaction", {
     id: text("id").primaryKey(),
-    
+    title: text("title").notNull(),
+    category: text("category").notNull(),
+    amount: bigint("amount", { mode: "number" }).notNull(),
+    expense_date: text("expense_date").notNull(),
+    created_at: bigint("created_at", { mode: "number" }).notNull(),
 });
