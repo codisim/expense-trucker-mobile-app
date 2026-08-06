63cf6d765fe510fda5a8b3966d2da4b7bdb9b2db
Waliullah <waliullah.godev@gmail.com>
2026-08-06 20:06:49 +0600

    expense screen

---
+++ app/(tabs)/expense.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ExpenseScreen = () => {
+  return (
+    <View>
+      <Text>ExpenseScreen</Text>
+    </View>
+  )
+}
+
+export default ExpenseScreen
