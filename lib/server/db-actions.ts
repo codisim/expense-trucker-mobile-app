b5e8173c4a7cbd71004943cc2997c12e212dcc7c
Waliullah <waliullah.godev@gmail.com>
2026-08-07 08:44:27 +0600

    updateExpenseItem

--- lib/server/db-actions.ts
+++ lib/server/db-actions.ts
@@ -1,4 +1,4 @@
-import { desc } from "drizzle-orm"
+import { desc, eq } from "drizzle-orm"
 import { db } from "./db/client"
 import { expenseTransaction } from "./db/schema"
 
@@ -14,13 +14,13 @@ export const getAllExpenses = async () => {
 
 
 
-export const createExpense = async (expense: {
+export const createExpenseItem = async (expense: {
     title: string;
     category: string;
     amount: number;
     expense_date: string;
 }) => {
-    const { title, category, amount, expense_date } = expense || {};  
+    const { title, category, amount, expense_date } = expense || {};
 
     const result = await db
         .insert(expenseTransaction)
@@ -35,4 +35,35 @@ export const createExpense = async (expense: {
         .returning();
 
     return result[0];
+};
+
+
+export const updateExpenseItem = async (
+    id: string,
+    expense: {
+        title: string;
+        category: string;
+        amount: number;
+        expense_date: string;
+    },
+) => {
+    const { title, category, amount, expense_date } = expense || {};
+
+    const result = await db
+        .update(expenseTransaction)
+        .set({
+            title,
+            category,
+            amount,
+            expense_date,
+        })
+        .where(eq(expenseTransaction.id, id))
+        .returning();
+
+    if(!result.length){
+        return null;
+    }
+
+    return result[0];
 }
+
