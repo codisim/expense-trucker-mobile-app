b67370deca45dad696e300b11850b16aca31905d
Waliullah <waliullah.godev@gmail.com>
2026-08-08 18:37:57 +0600

    PATCH

--- app/api/index+api.ts
+++ app/api/index+api.ts
@@ -1,7 +1,33 @@
+import { updateExpenseItem } from "@/lib/server/db-actions";
 
 
 export async function PATCH(request: Request, { id }: { id: string }) {
     try {
+
+        const body = await request.json();
+        const { title, category, expense_date, amount } = body || {};
+
+        const updateExpense = await updateExpenseItem(id, {
+            title,
+            category,
+            expense_date,
+            amount
+        })
+
+
+        if(!updateExpense){
+            return Response.json({
+                error: "item not found",
+                status: 404
+            });
+        }
+
+        return Response.json({
+            message: "Expense item updated successfully",
+            // data: updateExpense,
+            updateExpense,
+            status: 200
+        });
         
     } catch (error) {
         const errMessage = error instanceof Error ? error.message : "Failed to update expense item";
