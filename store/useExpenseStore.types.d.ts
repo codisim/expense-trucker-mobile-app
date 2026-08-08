7af5686ff4c37c4eaddb2558cc337900e2a79f68
Waliullah <waliullah.godev@gmail.com>
2026-08-08 20:38:26 +0600

    refactor: enhance expense store types and structure for better clarity

--- store/useExpenseStore.types.d.ts
+++ store/useExpenseStore.types.d.ts
@@ -18,3 +18,27 @@ export type IExpense = {
     //   expenseDate: Date;
     expenseDate: string;
 };
+
+
+export type IExpenseResponse = { expense: IExpense };
+
+
+export type IExpenseInput = {
+    title: string;
+    category: IExpenseCategory;
+    amount: number;
+    expenseDate: string;
+}
+
+
+export type IExpenseStore = {
+    userExpenses: IExpense[];
+    fetchExpenses: () => Promise<void>;
+    addExpense: (expense: IExpenseInput) => Promise<void>;
+    removeExpense: (id: string) => Promise<void>;
+    updateExpense: (id: string, input: IExpenseInput) => Promise<void>;
+
+    getExpensesByCategory: (category: IExpenseCategory) => IExpense[];
+    getTotalExpenses: () => number;
+    getTotalExpensesByCategory: (category: IExpenseCategory) => number;
+}
